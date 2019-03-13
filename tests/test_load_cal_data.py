0c62b6c5821e7130db4154edc121ba5bcf47e89e
elfnor <elfnor@gmail.com>
2019-03-13 16:44:12 +1300

    Issue #1 and #2 fixed

--- tests/test_load_cal_data.py
+++ tests/test_load_cal_data.py
@@ -5,7 +5,7 @@ import numpy as np
 caldata_fn = r'I:\MSL\Private\LENGTH\EQUIPREG\cal_data.xml'
 
 def test_load_cal_data():
-    red, green = load_cal_data.read_cal_wavelengths(caldata_fn, red_only=False)
+    red, green = load_cal_data.read_cal_wavelengths(caldata_fn, red_green=True)
     assert red[0] == 'LENGTH/2018/1111'
     np.testing.assert_allclose(red[1], 632.9913742, 0.0000001, )
     assert green[0] == 'MISEENPRATIQUE'
