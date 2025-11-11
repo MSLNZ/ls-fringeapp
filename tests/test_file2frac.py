e896a4eee83d2ca804bfa83d1a0b27a0094b94ba
elfnor <elfnor@gmail.com>
2025-11-11 16:32:42 +1300

    updated values in "fflog_to_test_2025-11-11.txt" to values produced by `array2frac`  today

--- tests/test_file2frac.py
+++ tests/test_file2frac.py
@@ -2,17 +2,25 @@
 test fringe fraction calculation against results from previous versions of software
 put a file fflog.txt with copied lines from calibration fflog.txt in /data
 put all corresponding cropped fringe images in /data
+
+
+EFH 2025-11-11
+I am updating the values in "fflog_to_test_2025-11-11.txt" to the fringe fraction values produced by the software today.
+This software has been validated by key comparison .....
 """
+
 from pathlib import Path
 import numpy as np
 from PIL import Image
 import fringeprocess as fp
 
-TEST_DATA_DIR = Path(__file__).resolve().parent / 'data'
+TEST_DATA_DIR = Path(__file__).resolve().parent / "data"
 
 
 def pytest_generate_tests(metafunc):
-    data = np.genfromtxt(TEST_DATA_DIR / 'fflog_to_test.txt', delimiter='\t', dtype='str')
+    data = np.genfromtxt(
+        TEST_DATA_DIR / "fflog_to_test_2025-11-11.txt", delimiter="\t", dtype="str"
+    )
     metafunc.parametrize("row", list(data))
 
 
@@ -20,7 +28,7 @@ def test_fringe_calc(row):
     ffrac_exp = np.float(row[0])
     img_filename = TEST_DATA_DIR / row[1]
     img = Image.open(img_filename)
-    img.convert('L')
+    img.convert("L")
     img_array = np.asarray(img)
     if img_array.ndim > 2:
         img_array = img_array.mean(axis=2)
@@ -30,4 +38,8 @@ def test_fringe_calc(row):
     # would have expected we could get 1e-4 here, but not always
     # algorithim must be slightly different
     # uncertainty analysis requires 0.4e-3 at 1 sigma,
-    np.testing.assert_allclose(ffrac_calc, ffrac_exp, 0.5e-3,)
+    np.testing.assert_allclose(
+        ffrac_calc,
+        ffrac_exp,
+        atol=0.00001,
+    )
