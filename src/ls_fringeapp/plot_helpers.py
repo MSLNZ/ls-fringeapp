f92cdb393e348e71457e28e376ce0c17855c6b00
elfnor <elfnor@gmail.com>
2025-12-04 11:24:13 +1300

    just about to fix synthetic_image code for ffrac  bug

--- src/ls_fringeapp/plot_helpers.py
+++ src/ls_fringeapp/plot_helpers.py
@@ -17,16 +17,17 @@ from ls_fringeapp import fringeprocess as fp
 
 
 # local version of FringeManager.annotate_fig
-def draw_gauge(axes, img_array, drawdata: dict):
+def draw_gauge(axes, img_array, drawdata: dict, peaks=True):
     axes.imshow(img_array, cmap=matplotlib.cm.gray)
     axes.axis("image")
     axes.plot(drawdata["xy"][:, 1], drawdata["xy"][:, 0], "or")
     axes.plot(drawdata["ccen"], drawdata["rcen"], "+c", ms=20)
     axes.plot(drawdata["co"], drawdata["ro"], "w-")
     axes.plot(drawdata["ci"], drawdata["ri"], "c-")
-    for col, peaks in enumerate(drawdata["pklist"]):
-        x = col * np.ones_like(peaks)
-        axes.plot(x, peaks, "+y")
+    if peaks:
+        for col, peaks in enumerate(drawdata["pklist"]):
+            x = col * np.ones_like(peaks)
+            axes.plot(x, peaks, "+y")
     maxx = img_array.shape[1]
     for cepts in drawdata["interceptsp"]:
         axes.plot([0, maxx], [cepts, drawdata["slopep"] * maxx + cepts], "-m", lw=2)
